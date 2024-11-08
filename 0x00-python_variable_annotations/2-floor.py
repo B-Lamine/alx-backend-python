ceaf6370b92cf9b01c1fb012fb06e72fc9b054de
B. Lamine <medlamine.bellilet@outlook.com>
2024-11-08 16:33:12 +0000

    2-floor.py: create file

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Module contains method:
+    - floor.
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """Return floor of input number.
+    """
+    return math.floor(n)
